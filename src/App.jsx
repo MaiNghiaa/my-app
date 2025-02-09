b831dc6ac137444fbbe743de6abe87ae3f2e29ae
Mainghiaa <nghia.mt210179P@sis.hust.edu.vn>
2025-02-09 11:43:25 +0700

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import React from 'react'
 // import Router from './routes/Router'
-import { BrowserRouter,  Router, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 
 import Home from "./pages/Home"
-import Chat  from './pages/Chat'
-import SignIn  from './pages/SignIn'
-import SignUp  from './pages/SignUp'
+import Chat from './pages/Chat'
+import SignIn from './pages/SignIn'
+import SignUp from './pages/SignUp'
 
 function App() {
-    return ( 
-
-
-        
-
+    return (
         <BrowserRouter>
-
-            <Router>
-        <Route path="/" element={<Home/>}></Route>
-       <Route path="/chat" element={<Chat/>}></Route>
-       <Route path="/signin" element={<SignIn/>}></Route>
-        <Route path="/signup" element={<SignUp/>}></Route>
-            </Router>
+            <Routes>
+                <Route path="/" element={<Home />}></Route>
+                <Route path="/chat" element={<Chat />}></Route>
+                <Route path="/signin" element={<SignIn />}></Route>
+                <Route path="/signup" element={<SignUp />}></Route>
+            </Routes>
         </BrowserRouter>
-
-          
-           
-     );
+    );
 }
 
 export default App;
